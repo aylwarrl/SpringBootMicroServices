45bf14348493bd829f3b47947f65524f2bd7d596
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-17 14:57:17 -0400

    javadoc

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,12 +3,26 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//need to add javadoc & commit
+
+/**
+ * @author Administrator
+ *
+ */
 @Controller
 public class PlantPlacesController {
 	
 	@RequestMapping("/start")
 	public String start() {
 		
+		return "start";
+	}
+	
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 */
+	public String index() {
 		return "start";
 	}
 	
